e068e54f1963c7120bdc71065bab0be64554e77a
Nursultan <nurlanov0105@gmail.com>
2024-02-19 18:03:01 +0600

    small modificate in Skeleton

--- src/shared/ui/detailsSkeleton/DetailsSkeleton.jsx
+++ src/shared/ui/detailsSkeleton/DetailsSkeleton.jsx
@@ -6,19 +6,21 @@ const DetailsSkeleton = () => {
       <>
          <div className={styles.CommonSection}></div>
          <div className={styles.PlaceInfo}>
-            <div className={styles.heading}>
-               <h1 className={styles.title}></h1>
-               <div className={styles.location}></div>
-            </div>
-            <div className={styles.descr}>
-               <h3></h3>
-               <p></p>
-            </div>
-            <div className={styles.reviews}>
-               <h3></h3>
-               <div className={styles.list}>
-                  <div className={styles.review}></div>
-                  <div className={styles.review}></div>
+            <div className='container'>
+               <div className={styles.heading}>
+                  <h1 className={styles.title}></h1>
+                  <div className={styles.location}></div>
+               </div>
+               <div className={styles.descr}>
+                  <h3></h3>
+                  <p></p>
+               </div>
+               <div className={styles.reviews}>
+                  <h3></h3>
+                  <div className={styles.list}>
+                     <div className={styles.review}></div>
+                     <div className={styles.review}></div>
+                  </div>
                </div>
             </div>
          </div>
